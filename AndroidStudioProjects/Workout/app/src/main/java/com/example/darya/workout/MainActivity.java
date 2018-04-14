59cffea7460aa3cba0d9c5c21b445efb72c5fcb4
Dar'ya Kolpakova <daka.kolp01@gmail.com>
2018-04-14 16:43:23 +0300

    Workout: Fragment, Transact

--- AndroidStudioProjects/Workout/app/src/main/java/com/example/darya/workout/MainActivity.java
+++ AndroidStudioProjects/Workout/app/src/main/java/com/example/darya/workout/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.darya.workout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WorkoutDetailFragment fragment = (WorkoutDetailFragment) getFragmentManager().findFragmentById(R.id.detail_frag);
-        fragment.setWorkoutId(1);
+//        WorkoutDetailFragment fragD = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+//        fragD.setWorkoutId(1);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkoutId(id);
+        ft.replace(R.id.fragment_container, details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+        ft.commit();
+
     }
 }
--- AndroidStudioProjects/Workout/app/src/main/java/com/example/darya/workout/WorkoutDetailFragment.java
+++ AndroidStudioProjects/Workout/app/src/main/java/com/example/darya/workout/WorkoutDetailFragment.java
@@ -2,10 +2,11 @@ package com.example.darya.workout;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -30,4 +31,16 @@ public class WorkoutDetailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if(view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getNameEx());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
 }
